65f44a32efb82bff48dabc9f0852dbcac25900be
Adam Liechty <adam@doubledutch.me>
2019-02-05 08:47:46 -0800

    Looking...

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
-export default () => (
-  <div>
+const NotFound = () => (
+  <div style={{margin: "1em auto", textAlign: 'center'}}>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn't exist... the sadness.</p>
   </div>
 )
+
+const Loading = () => (
+  <div style={{margin: "1em auto", textAlign: 'center'}}>
+    <p>Looking...</p>
+  </div>
+)
+
+export default class extends PureComponent {
+  state = { C: Loading }
+
+  componentDidMount() {
+    this.timer = setTimeout(() => this.setState({C: NotFound}), 500)
+  }
+
+  render() {
+    const { C } = this.state
+    return <C />
+  }
+}
